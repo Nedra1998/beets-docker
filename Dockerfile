b9f2044d834dd3097e1713182243d3a595b4a0d2
Arden Rasmussen <ardenisthebest@gmail.com>
2025-12-31 08:34:44 -0800

    feat: improve entrypoint and reduce final image size

--- Dockerfile
+++ Dockerfile
@@ -1,103 +1,33 @@
-FROM alpine:3.21 AS main
+FROM alpine:3
 
 LABEL name="docker-beets" \
-      maintainer="Arden Rasmussen" \
-      description="Beets is the media library management system for obsessive music geeks." \
-      url="https://beets.io" \
-      org.label-schema.vcs-url="http://vega.lyra:9070/Arden/beets-docker" \
-      org.opencontainers.image.source="http://vega.lyra:9070/Arden/beets-docker"
-
-COPY rootfs /
+  maintainer="Arden Rasmussen" \
+  description="Beets is the media library management system for obsessive music geeks." \
+  url="https://beets.io" \
+  org.label-schema.vcs-url="http://github.com/Nedra1998/beets-docker" \
+  org.opencontainers.image.source="http://github.com/Nedra1998/beets-docker"
 
 WORKDIR /src
 
-ENV HOME=/config \
-    BEETSDIR=/config/.config/beets \
-    FPCALC=/usr/bin/fpcalc \
-    UMASK_SET=022 \
-    TZ=America/Los_Angeles
-
-RUN apk update && \
-    apk upgrade && \
-    apk add --no-cache --virtual=base --upgrade \
-        bash \
-	    bash-completion \
-        vim \
-        curl \
-        wget \
-        ca-certificates \
-        coreutils \
-        procps \
-        tar \
-        xz \
-        gzip \
-        tzdata && \
+RUN apk update && apk upgrade && \
     apk add --no-cache --virtual=build-dependencies --upgrade \
-        build-base \
-        git \
-        zip \
-        make \
-        cmake \
-        g++ \
-        gcc \
-        musl-dev \
-        cargo \
-        libffi-dev \
-        zlib-dev \ 
-        python3-dev \
-        openssl-dev \
-        jpeg-dev \
-        libpng-dev \
-        mpg123-dev \
-        ffmpeg-dev \
-        fftw-dev && \
-    apk add --upgrade --no-cache \
-        python3 \
-        py3-pip \
-        inotify-tools \
-        chromaprint \
-        expat \
-        ffmpeg \
-        ffmpeg-libs \
-        fftw \
-        flac \
-        gdbm \
-        gst-plugins-good \
-        gstreamer \
-        py3-gobject3 \
-        lame \
-        libffi \
-        libev \
-        mpg123 \
-        imagemagick \
-        jpeg \
-        libpng \
-        openjpeg \
-        sqlite-libs \
-        keyfinder-cli
-
-RUN apk add --no-cache --virtual=mp3gain --upgrade --repository="https://dl-cdn.alpinelinux.org/alpine/edge/testing/" \
-        mp3gain \
-        mp3val && \
+      build-base cairo-dev cargo cmake ffmpeg-dev fftw-dev git \
+      gobject-introspection-dev jpeg-dev libpng-dev mpg123-dev openjpeg-dev \
+      python3-dev && \
+    apk add --no-cache \
+      bash chromaprint expat ffmpeg fftw flac gdbm gobject-introspection \
+      gst-plugins-good gstreamer imagemagick jpeg lame libffi libpng mpg123 \
+      nano openjpeg python3 py3-pip sqlite-libs mp3gain inotify-tools && \
+    apk add --no-cache --virtual=mp3gain --upgrade --repository="https://dl-cdn.alpinelinux.org/alpine/edge/testing/" \
+      mp3val && \
     pip3 install --no-cache-dir --upgrade --break-system-packages \
-        https://github.com/beetbox/beets/tarball/master \
-        #https://github.com/Holzhaus/beets-extrafiles/tarball/master \
-        https://github.com/jee-r/beets-extrafiles/tarball/main \
-        beetcamp \
-        python3-discogs-client \
-        beets-lidarr-fields \
-        beets-noimport \
-        pyacoustid \
-        wheel \
-        requests \
-        beautifulsoup4 \
-        pillow \
-        unidecode \
-        pylast && \
-    # install Beet Bash completion
-    beet completion > /usr/share/bash-completion/completions/beet && \
-    chmod +x /usr/local/bin/entrypoint.sh && \
+      'beets[chroma,embedart,fetchart,lastgenre,lyrics,replaygain] @ git+https://github.com/beetbox/beets.git' \
+      beetcamp beets-copyartifacts3 && \
     apk del --purge build-dependencies && \
-    rm -rf /tmp/* /pkgs ~/.cache
+    rm -rf /tmp/* /pkgs $HOME/.cache $HOME/.cargo
+
+COPY rootfs /
+ENV BEETSDIR=/config
+RUN chmod +x /usr/local/bin/entrypoint.sh
 
 CMD ["/usr/local/bin/entrypoint.sh"]
--- rootfs/usr/local/bin/entrypoint.sh
+++ rootfs/usr/local/bin/entrypoint.sh
@@ -1,61 +1,140 @@
 #!/bin/bash
+# @file entrypoint.sh
+# @brief Entrypoint script for the Beets docker container.
+# @description
+# The tnrypoint script supports the main mode of operation for the docker
+# container, and handles monitoring for file changes in order to trigger beets
+# imports. It also supports running arbitrary beet commands directly, by providing
+# arguments to the container.
+#
+# The configuration for the entrypoint script is done via environment
+# variables, which control the two main modes of operation (which can be used
+# at the same time):
+#
+# **Trigger file monitoring**: If the `TRIGGER_FILE` environment variable is
+# set, it will monitor that file for modifications using inotify. When the file
+# is modified, it will run `beet import` on the specified LIBRARY_DIR. This is
+# is useful for triggering imports from external processes by simply touching
+# the trigger file. And it will do an in-place import not moving any files,
+# especially useful for files that are managed by a download manager or
+# similar.
+#
+# **Import directory monitoring**: If the `IMPORT_DIR` environment variable is
+# set, it will monitor that directory for new or modified files using inotify.
+# When changes are detected, it will wait for a debounce period of inactivity
+# set by `DEBOUNCE_SECONDS` before running `beet import` on the directory. This
+# is useful for automatically importing new music files into your beets library
+# as they are added, either manually or automatically to the import directory.
 
-# Set the default configuration file
-if [ ! -d "/config" ]; then
-  mkdir -p "/config"
+
+TRIGGER_FILE="${TRIGGER_FILE:-}"
+LIBRARY_DIR="${LIBRARY_DIR:-/library}"
+IMPORT_DIR="${IMPORT_DIR:-}"
+DEBOUNCE_SECONDS="${DEBOUNCE_SECONDS:-15}"
+CONFIG_FILE="${CONFIG_FILE:-/config/config.yaml}"
+
+LOCK_DIR="/tmp/beets_import.lock"
+
+# @description Log a message with a timestamp.
+#
+# @arg $1 Log level (e.g., TRC, DBG, INF, WRN, ERR, FTL).
+# @arg $2 Log message.
+#
+# @stderr The log message is printed to standard error, with the current date
+# and time.
+log() {
+  printf "%s %s %s\n" "$(date +%Y-%m-%dT%H:%M:%S)" "$1" "$2" >&2
+}
+
+# @description Acquire a POSIX atomic lock using mkdir.
+# The lock is represented by a directory. If the lock directory exists,
+# then the lock is held by another process, and this function will wait
+# until it can acquire the lock.
+#
+# **Warning**: This function will block indefinitely until the lock is
+# acquired, always remember to release the lock after acquiring it, even
+# in error conditions.
+acquire_lock() {
+    while ! mkdir "$LOCK_DIR" 2>/dev/null; do
+        sleep 1
+    done
+}
+
+
+# @description Release the POSIX atomic lock.
+release_lock() {
+    rmdir "$LOCK_DIR" 2>/dev/null
+}
+
+# @description Monitor the trigger file for modifications and run beets import
+# on the `LIBRARY_DIR` when it changes.
+monitor_trigger_file() {
+  log INF "Starting trigger file watcher on '$TRIGGER_FILE' for import from '$LIBRARY_DIR'."
+
+  inotifywait -m -e close_write --format '%w%f' "$TRIGGER_FILE" | while read -r FILE; do
+    log DBG "Trigger file '$FILE' modified, running import on '$LIBRARY_DIR'."
+    acquire_lock
+    beet -c "${CONFIG_FILE}" import --quiet --nocopy "$LIBRARY_DIR"
+    release_lock
+    log INF "Import completed for '$LIBRARY_DIR'. Returning to watch mode."
+  done
+}
+
+# @description Monitor the import directory for new or modified files and run
+# beets import after a debounce period of inactivity. The debounce period is
+# a period of time during which no new file events are detected, after which
+# the import is triggered. This is to prevent multiple imports from being triggered
+# while files are still being added.
+monitor_import_directory() {
+  log INF "Starting import directory watcher on '$IMPORT_DIR' with debounce of ${DEBOUNCE_SECONDS}s."
+
+  # TODO: Currently depends on bash process substitution, would be better to avoid
+  # the dependency for better portability, and only use POSIX features.
+  exec 3< <(inotifywait -m -r -e create,modify,move,moved_to,close_write --format '%w%f' "$IMPORT_DIR")
+
+  while read -r EVENT <&3; do
+    log DBG "Event detected '$EVENT', waiting for debounce period of ${DEBOUNCE_SECONDS}s."
+
+    # Wait for the debounce period
+    # TODO: Currently this depends on bash-specific features, would be better to avoid
+    # and only use POSIX features for better portability.
+    while read -r -t "$DEBOUNCE_SECONDS" EVENT <&3; do
+      log DBG "Additional event detected '$EVENT', resetting debounce timer."
+      true
+    done
+
+    acquire_lock
+    beet -c "${CONFIG_FILE}" import --quiet "$IMPORT_DIR"
+    release_lock
+    log INF "Import completed for '$IMPORT_DIR'. Returning to watch mode."
+  done
+}
+
+# @description Cleanup function to stop monitors and exit gracefully.
+# This is the callback for the trap on termination signals which the container
+# will receive when stopping the docker container.
+cleanup() {
+  log INF "Stopping monitors and exiting..."
+  # Kill all background jobs (the watcher loops)
+  kill $(jobs -p) 2>/dev/null
+  release_lock
+  exit 0
+}
+
+# Trap TERM and INT signals
+trap cleanup TERM INT
+
+if [ $# -gt 0 ]; then
+  exec beet -c "${CONFIG_FILE}" "$@"
+  exit $?
 fi
 
-if [ -w /config ] && [ ! -f /config/config.yaml ]; then
-  cp /etc/default/beets_config.yaml /config/config.yaml
+if [ -n "$TRIGGER_FILE" ]; then
+  monitor_trigger_file &
 fi
 
-beets_config="/config/config.yaml"
+if [ -n "$IMPORT_DIR" ]; then
+  monitor_import_directory &
+fi
 
-inotifywait -m -e create,moved_to --format '%w%f' "$WATCH_DIR" | while IFS= read -r dir_path; do
-  sleep 1
-  # Wait for file to be completely written...
-  if [ -f "$dir_path" ]; then
-    printf "Waiting for file to be completely written: %s\n" "$dir_path"
-    # Wait for file to be completely written by checking for size changes
-    last_size=$(stat -c%s "$dir_path")
-    sleep 1
-    while [ "$last_size" -ne "$(stat -c%s "$dir_path")" ]; do
-      last_size=$(stat -c%s "$dir_path")
-      sleep 1
-    done
-  elif [ -d "$dir_path" ]; then
-    printf "Waiting for directory to be completely written: %s\n" "$dir_path"
-    # Wait for directory to be completely written by checking for new files
-    last_count=$(find "$dir_path" -type f | wc -l)
-    sleep 1
-    while [ "$last_count" -ne "$(find "$dir_path" -type f | wc -l)" ]; do
-      last_count=$(find "$dir_path" -type f | wc -l)
-      sleep 1
-    done
-  fi
-
-  # Unzip zip files into new subdirectories
-  if [ -f "$dir_path" ]; then
-    if [[ "$dir_path" == *.zip ]]; then
-      unzip -d "${dir_path%.*}" "$dir_path"
-      rm "$dir_path"
-
-      continue
-    fi
-  fi
-
-  sleep 1
-
-  # Import music files
-  printf "Importing music files from %s\n" "$dir_path"
-  /usr/bin/beet -c "$beets_config" import --quiet --incremental --flat "$dir_path"
-
-  # Cleanup any files that weren't moved
-  if [ -d "$dir_path" ]; then
-    rm -r "$dir_path"
-  else
-    rm "$dir_path"
-  fi
-done
-
-exit 1
+wait
